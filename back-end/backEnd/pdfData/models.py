8e8d71ac9e0d21df77ff940cb12e851a679ea179
Dana Yuan <danayuan2006@gmail.com>
2025-08-01 09:32:33 -0400

    Added basic code to pdfData model

--- back-end/backEnd/pdfData/models.py
+++ back-end/backEnd/pdfData/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class pdfData(models.Model):
+    id = models.TextField()
+    name = models.TextField()
+    numPages = models.IntegerField()
+
+    def __str__(self):
+        return self.title
